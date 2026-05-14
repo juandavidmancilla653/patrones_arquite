565dd9c4b61f605238c0cc2768741a592b121d77
juan david <juandavidmancilla653@gmail.com>
2026-05-14 18:17:37 -0500

    Agregar clase UsuarioMicroservicio con estructura inicial

---
+++ arquitectura/src/main/java/com/patrones/microservicios/usuarios/UsuarioMicroservicio.java
@@ -0,0 +1,11 @@
+package com.patrones.microservicios.usuarios;
+
+public class UsuarioMicroservicio {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Microservicio Usuarios"
+        );
+    }
+}
