e41ca7c99bd010bcf4327981d5129a1d915d207e
juan david <juandavidmancilla653@gmail.com>
2026-05-14 18:15:40 -0500

    Agregar clase ProductoMicroservicio con estructura inicial

---
+++ arquitectura/src/main/java/com/patrones/microservicios/productos/ProductoMicroservicio.java
@@ -0,0 +1,11 @@
+package com.patrones.microservicios.productos;
+
+public class ProductoMicroservicio {
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                "Microservicio Productos"
+        );
+    }
+}
