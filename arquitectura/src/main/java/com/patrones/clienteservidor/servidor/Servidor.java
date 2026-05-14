865180cb0a5f5cde6f8206793d6d4a7c277d24bb
juan david <juandavidmancilla653@gmail.com>
2026-05-14 18:08:48 -0500

    Agregar clase Servidor vacía

---
+++ arquitectura/src/main/java/com/patrones/clienteservidor/servidor/Servidor.java
@@ -0,0 +1,9 @@
+package com.patrones.clienteservidor.servidor;
+
+public class Servidor {
+
+    public static void main(String[] args) {
+
+        System.out.println("Servidor iniciado");
+    }
+}
